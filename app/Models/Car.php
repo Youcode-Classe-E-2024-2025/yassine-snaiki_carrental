9e341337560cf48df91299b61d8103b7c140c1ed
yassine snaiki <yassnaiki@gmail.com>
2025-03-10 13:04:58 +0000

    models

---
+++ app/Models/Car.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    /** @use HasFactory<\Database\Factories\CarFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'year',
+        'price_per_day',
+        'is_available',
+    ];
+    public function rentals() {
+        return $this->hasMany(Rental::class);
+    }
+
+}
